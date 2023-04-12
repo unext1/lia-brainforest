bd1eb9ea113700ed19fe3a306c1bc55c36960c98
Richard Johannesson <rcj@Richards-MacBook-Pro-2.local>
2023-04-12 16:02:42 +0200

    added button component

---
+++ app/components/button.tsx
@@ -0,0 +1,32 @@
+import { Link } from '@remix-run/react';
+
+const Btn = ({
+    title,
+    link,
+    href,
+}: {
+    title: string;
+    link: boolean;
+    href: string;
+}) => {
+    return (
+        <>
+            {link ? (
+                <Link
+                    to={href}
+                    className='w-fit flex sm:px-12 sm:py-2.5 mx-auto mt-8 text-xs px-6 py-1.5 md:text-sm font-bold text-white uppercase bg-red-500 rounded-lg'
+                >
+                    {title}
+                </Link>
+            ) : (
+                <button className=' w-fit flex sm:px-12 sm:py-2.5 mx-auto mt-8  text-xs px-6 py-1.5 md:text-sm font-bold text-white uppercase bg-red-500 rounded-lg'>
+                    {title}
+                </button>
+            )}
+        </>
+    );
+};
+
+export default Btn;
+
+// <Btn title='Try it' link href='/dashboard' />
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,64 +1,61 @@
-import { Link } from "@remix-run/react";
-import InfoCards from "~/components/infoCards";
+import { Link } from '@remix-run/react';
+import Btn from '~/components/button';
+import InfoCards from '~/components/infoCards';
 
 export default function Index() {
-  return (
-    <>
-      <div className="py-20 bg-black ">
-        <div className="container mx-auto max-w-7xl">
-          <div>
-            <img
-              className="mb-10 opacity-95 rounded-3xl"
-              src="https://www.upgrad.com/blog/wp-content/uploads/2019/11/118-banner.png"
-              alt=""
-            />
-          </div>
-        </div>
-      </div>
-      <div className="container px-5 pb-40 mx-auto max-w-7xl">
-        <div>
-          <h1 className="mt-20 text-4xl font-semibold text-center lg:text-5xl">
-            Why should you use us
-          </h1>
-          <p className="mt-2 text-sm text-center text-gray-600 lg:text-base ">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia ea
-            quos, iste culpa nam eligendi aliquam
-          </p>
-        </div>
+    return (
+        <>
+            <div className='py-20 bg-black '>
+                <div className='container mx-auto max-w-7xl'>
+                    <div>
+                        <img
+                            className='mb-10 opacity-95 rounded-3xl'
+                            src='https://www.upgrad.com/blog/wp-content/uploads/2019/11/118-banner.png'
+                            alt=''
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className='container px-5 pb-40 mx-auto max-w-7xl'>
+                <div>
+                    <h1 className='mt-20 text-4xl font-semibold text-center lg:text-5xl'>
+                        Why should you use us
+                    </h1>
+                    <p className='mt-2 text-sm text-center text-gray-600 lg:text-base '>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                        elit. Officia ea quos, iste culpa nam eligendi aliquam
+                    </p>
+                </div>
 
-        <InfoCards
-          alignRight={false}
-          title="Create a share vision."
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia ea quos, iste culpa nam eligendi aliquam Lorem ipsum
+                <InfoCards
+                    alignRight={false}
+                    title='Create a share vision.'
+                    description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia ea quos, iste culpa nam eligendi aliquam Lorem ipsum
             dolor, sit amet consectetur adipisicing elit. Officia ea quos,
-            iste culpa nam eligendi aliquam Lorem ipsum dolor, sit amet"
-        />
-        <InfoCards
-          alignRight
-          title="Something different."
-          description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia ea quos, iste culpa nam eligendi aliquam Lorem ipsum
+            iste culpa nam eligendi aliquam Lorem ipsum dolor, sit amet'
+                />
+                <InfoCards
+                    alignRight
+                    title='Something different.'
+                    description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia ea quos, iste culpa nam eligendi aliquam Lorem ipsum
             dolor, sit amet consectetur adipisicing elit. Officia ea quos,
-            iste culpa nam eligendi aliquam Lorem ipsum dolor, sit amet"
-        />
-        <div className="px-10 py-8 mx-auto mt-20 bg-black sm:py-16 sm:px-20 rounded-3xl">
-          <div className="container mx-auto">
-            <h1 className="font-bold tracking-wider text-center text-white sm:text-4xl md:text-5xl ">
-              Try out our SEO enhancer.
-            </h1>
-            <p className="mt-5 text-xs text-center text-gray-400 sm:text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
-              ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
+            iste culpa nam eligendi aliquam Lorem ipsum dolor, sit amet'
+                />
+                <div className='px-10 py-8 mx-auto mt-20 bg-black sm:py-16 sm:px-20 rounded-3xl'>
+                    <div className='container mx-auto'>
+                        <h1 className='font-bold tracking-wider text-center text-white sm:text-4xl md:text-5xl '>
+                            Try out our SEO enhancer.
+                        </h1>
+                        <p className='mt-5 text-xs text-center text-gray-400 sm:text-sm'>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Lorem ipsum dolor sit amet consectetur
+                            adipisicing elit.
+                        </p>
 
-            <Link
-              to="/dashboard"
-              className=" w-fit flex sm:px-12 sm:py-2.5 mx-auto mt-8  text-xs px-6 py-1.5 md:text-sm font-bold text-white uppercase bg-red-500 rounded-lg"
-            >
-              Try It
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+                        <Btn title='Try it' link href='/dashboard' />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
