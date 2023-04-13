1c833f71987457ce2e44aef0b91716cd96b8a97d
laurynas <unextbusinessman@gmail.com>
2023-04-13 13:48:24 +0200

    new dashboard

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -7,3 +7,222 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+// import { Fragment, useState } from "react";
+// import { Dialog, Transition } from "@headlessui/react";
+// import { Link, Outlet } from "@remix-run/react";
+
+// const navigation = [
+//   { name: "Dashboard", href: "/dashboard", current: true },
+//   { name: "Images", href: "/dashboard/image", current: true },
+// ];
+// const teams = [
+//   { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
+//   { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
+//   { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
+// ];
+
+// function classNames(...classes: any) {
+//   return classes.filter(Boolean).join(" ");
+// }
+
+// export default function Example() {
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+//   return (
+//     <>
+//       <div>
+//         <Transition.Root show={sidebarOpen} as={Fragment}>
+//           <Dialog
+//             as="div"
+//             className="relative z-50 lg:hidden"
+//             onClose={setSidebarOpen}
+//           >
+//             <Transition.Child
+//               as={Fragment}
+//               enter="transition-opacity ease-linear duration-300"
+//               enterFrom="opacity-0"
+//               enterTo="opacity-100"
+//               leave="transition-opacity ease-linear duration-300"
+//               leaveFrom="opacity-100"
+//               leaveTo="opacity-0"
+//             >
+//               <div className="fixed inset-0 bg-gray-900/80" />
+//             </Transition.Child>
+
+//             <div className="fixed inset-0 flex">
+//               <Transition.Child
+//                 as={Fragment}
+//                 enter="transition ease-in-out duration-300 transform"
+//                 enterFrom="-translate-x-full"
+//                 enterTo="translate-x-0"
+//                 leave="transition ease-in-out duration-300 transform"
+//                 leaveFrom="translate-x-0"
+//                 leaveTo="-translate-x-full"
+//               >
+//                 <Dialog.Panel className="relative flex flex-1 w-full max-w-xs mr-16">
+//                   <Transition.Child
+//                     as={Fragment}
+//                     enter="ease-in-out duration-300"
+//                     enterFrom="opacity-0"
+//                     enterTo="opacity-100"
+//                     leave="ease-in-out duration-300"
+//                     leaveFrom="opacity-100"
+//                     leaveTo="opacity-0"
+//                   >
+//                     <div className="absolute top-0 flex justify-center w-16 pt-5 left-full">
+//                       <button
+//                         type="button"
+//                         className="-m-2.5 p-2.5"
+//                         onClick={() => setSidebarOpen(false)}
+//                       >
+//                         <span className="sr-only">Close sidebar</span>
+//                         <div className="w-6 h-6 text-white" aria-hidden="true">
+//                           X
+//                         </div>
+//                       </button>
+//                     </div>
+//                   </Transition.Child>
+//                   {/* Sidebar component, swap this element with another sidebar if you like */}
+//                   <div className="flex flex-col px-6 pb-4 overflow-y-auto bg-white grow gap-y-5">
+//                     <div className="flex items-center h-16 shrink-0">
+//                       <img
+//                         className="w-auto h-8"
+//                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+//                         alt="Your Company"
+//                       />
+//                     </div>
+//                     <nav className="flex flex-col flex-1">
+//                       <div className="flex flex-col flex-1 gap-y-7">
+//                         <div className="-mx-2 space-y-1">
+//                           {navigation.map((item) => (
+//                             <div key={item.name}>
+//                               <Link
+//                                 to={item.href}
+//                                 className={classNames(
+//                                   item.current
+//                                     ? "bg-gray-50 text-indigo-600"
+//                                     : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+//                                   "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+//                                 )}
+//                               >
+//                                 <div>ICON</div>
+//                                 {item.name}
+//                               </Link>
+//                             </div>
+//                           ))}
+//                         </div>
+//                         <div>
+//                           <div className="text-xs font-semibold leading-6 text-gray-400">
+//                             Your teams
+//                           </div>
+//                           <div className="mt-2 -mx-2 space-y-1">
+//                             {teams.map((team) => (
+//                               <div key={team.name}>
+//                                 <a
+//                                   href={team.href}
+//                                   className={classNames(
+//                                     team.current
+//                                       ? "bg-gray-50 text-indigo-600"
+//                                       : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+//                                     "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+//                                   )}
+//                                 >
+//                                   <span
+//                                     className={classNames(
+//                                       team.current
+//                                         ? "text-indigo-600 border-indigo-600"
+//                                         : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
+//                                       "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
+//                                     )}
+//                                   >
+//                                     {team.initial}
+//                                   </span>
+//                                   <span className="truncate">{team.name}</span>
+//                                 </a>
+//                               </div>
+//                             ))}
+//                           </div>
+//                         </div>
+//                         <div className="mt-auto">
+//                           <a
+//                             href="/link"
+//                             className="flex p-2 -mx-2 text-sm font-semibold leading-6 text-gray-700 rounded-md group gap-x-3 hover:bg-gray-50 hover:text-indigo-600"
+//                           >
+//                             ICON Settings
+//                           </a>
+//                         </div>
+//                       </div>
+//                     </nav>
+//                   </div>
+//                 </Dialog.Panel>
+//               </Transition.Child>
+//             </div>
+//           </Dialog>
+//         </Transition.Root>
+
+//         {/* Static sidebar for desktop */}
+//         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+//           {/* Sidebar component, swap this element with another sidebar if you like */}
+//           <div className="flex flex-col px-6 pb-4 overflow-y-auto bg-white border-r border-gray-200 grow gap-y-5">
+//             <div className="flex items-center h-16 shrink-0">
+//               <img
+//                 className="w-auto h-8"
+//                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+//                 alt="Your Company"
+//               />
+//             </div>
+//             <nav className="flex flex-col flex-1">
+//               <div className="flex flex-col flex-1 gap-y-7">
+//                 <div className="-mx-2 space-y-1">
+//                   {navigation.map((item) => (
+//                     <div key={item.name}>
+//                       <a
+//                         href={item.href}
+//                         className={classNames(
+//                           item.current
+//                             ? "bg-gray-50 text-indigo-600"
+//                             : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+//                           "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+//                         )}
+//                       >
+//                         <div>ICON</div>
+//                         {item.name}
+//                       </a>
+//                     </div>
+//                   ))}
+//                 </div>
+
+//                 <div className="mt-auto">
+//                   <a
+//                     href="/"
+//                     className="flex p-2 -mx-2 text-sm font-semibold leading-6 text-gray-700 rounded-md group gap-x-3 hover:bg-gray-50 hover:text-indigo-600"
+//                   >
+//                     <div>SETTINGSICON</div>
+//                     Settings
+//                   </a>
+//                 </div>
+//               </div>
+//             </nav>
+//           </div>
+//         </div>
+//         <button
+//           type="button"
+//           className="-m-2.5 p-2.5 text-gray-700 lg:hidden absolute left-10 top-5"
+//           onClick={() => setSidebarOpen(true)}
+//         >
+//           <span className="sr-only">Open sidebar</span>
+//           OPEN
+//         </button>
+
+//         <div className="lg:pl-72">
+//           <main className="py-10 mt-20 lg:mt-0">
+//             <div className="px-4 sm:px-6 lg:px-8">
+//               <Outlet />
+//             </div>
+//           </main>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
