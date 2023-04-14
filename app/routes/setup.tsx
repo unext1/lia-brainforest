0db892cf80d44fc72e9d101451d74a5525c91809
IndieAK <kindbergadam@gmail.com>
2023-04-14 08:16:41 +0200

    changed setup

--- app/routes/setup.tsx
+++ app/routes/setup.tsx
@@ -58,11 +58,7 @@ export const loader: LoaderFunction = async ({ request }) => {
   const cookieHeader = request.headers.get("Cookie");
   const cookie = (await wordpressCookie.parse(cookieHeader)) || null;
 
-  if (!cookie) return { cookie: null };
-  if (!cookie.url) return { url: null, username: null };
-  if (!cookie.username) return { url: null, username: null };
-
-  return { url: cookie.url, username: cookie.username, cookie: true };
+  return { url: cookie ? cookie.url : false, cookie: cookie ? true : false };
 };
 const isValidUrl = (urlString: string) => {
   try {
@@ -75,71 +71,91 @@ export default function Setup() {
   const actionData = useActionData();
   const loaderData = useLoaderData();
   return (
-    <div className="flex flex-col items-center justify-center gap-10 pt-6 mx-10 font-mono md:items-start md:flex-row md:justify-start ">
-      <div className=" max-w-[50%] min-h-[200px] flex-1">
-        <h2 className="text-2xl font-semibold leading-10 ">Getting started</h2>
-        <p>Step 1: Install WP Rest Api Plugin in wordpress plugins.</p>
-        <p>
-          Step 2: Press the{" "}
-          <a className="text-blue-400" href="/jwt-secret-plugin.zip" download>
-            download link
-          </a>{" "}
-          and add the zip file to plugins
-        </p>
-        <p>Step 3: Enter your wordpress url, username and password </p>
-        <img
-          src="/setup illustration.png"
-          alt="illustration of setup"
-          className="hidden md:block"
-        />
-      </div>
-      <div className=" min-h-[200px] max-w-xs ">
-        {!loaderData.cookie ? (
-          <Form className="flex flex-col gap-2 " method="post">
-            <h4 className="text-xl font-semibold">Connect your wordpress</h4>
-            <input
-              name="url"
-              className="mt-2 border rounded-sm"
-              placeholder={loaderData.url ? loaderData.url : "wordpress url"}
-            />
-            <input
-              name="username"
-              className="border rounded-sm"
-              placeholder={
-                loaderData.username ? loaderData.username : "username"
-              }
-            />
-            <input
-              name="password"
-              type="password"
-              className="border rounded-sm "
-              placeholder="password"
-            />
-            <button
-              className="w-fit duration-150 transform hover:scale-105 flex sm:px-12 sm:py-2.5 mx-auto mt-2  text-xs px-6 py-1.5 md:text-sm font-bold text-white uppercase bg-blue-500 rounded-lg"
-              type="submit"
+    <div className="h-full ">
+      <div className=" max-h-[600px] md:h-[520px] flex flex-col items-center justify-center gap-10 pt-6 mx-10 font-mono md:items-start md:flex-row md:justify-start ">
+        <div className=" md:max-w-[50%] min-h-[200px] h-ful rounded-lg pl-4 py-4 pb-8 md:py-0 md:pb-0 flex-col flex">
+          <h2 className="text-2xl font-semibold leading-10 ">
+            Getting started
+          </h2>
+          <p>
+            Step 1: Install{" "}
+            <a
+              href="https://sv.wordpress.org/plugins/jwt-authentication-for-wp-rest-api/"
+              className="text-blue-400"
             >
-              Log in
-            </button>
-          </Form>
-        ) : (
-          <h5>
-            Hi there, you are already setup! But feel free to check the steps
-            again.
-          </h5>
-        )}
-        {
-          <div className="mt-10 text-sm ">
-            <p className="">
-              *NOTE* We do not use your username and password for anything
-              except changing the wordpress image SEO.
-            </p>
+              WP Rest Api Plugin
+            </a>{" "}
+            in wordpress plugins.
+          </p>
+          <p>
+            Step 2: Download our{" "}
+            <a className="text-blue-400" href="/jwt-secret-plugin.zip" download>
+              jwt secret plugin
+            </a>{" "}
+            and add the zip file to plugins
+          </p>
+          <p>Step 3: Enter your wordpress url, username and password </p>
+          <img
+            src="/setup illustration.png"
+            alt="illustration of setup"
+            className=" max-w-[430px] max-h-[18rem] hidden md:block"
+          />
+        </div>
+        <div className=" min-h-[200px] md:max-w-sm px-4 py-4 md:py-0 rounded-lg h-full">
+          {!loaderData.cookie ? (
+            <Form className="flex flex-col gap-2 " method="post">
+              <h4 className="text-xl font-semibold">Connect your wordpress</h4>
+              <input
+                name="url"
+                className="mt-2 border rounded-sm"
+                placeholder={loaderData.url ? loaderData.url : "wordpress url"}
+              />
+              <input
+                name="username"
+                className="border rounded-sm"
+                placeholder={
+                  loaderData.username ? loaderData.username : "username"
+                }
+              />
+              <input
+                name="password"
+                type="password"
+                className="border rounded-sm"
+                placeholder="password"
+              />
+              <button
+                className="w-fit duration-150 transform hover:scale-105 flex sm:px-12 sm:py-2.5 mx-auto mt-2  text-xs px-6 py-1.5 md:text-sm font-bold text-white uppercase bg-blue-500 rounded-lg"
+                type="submit"
+              >
+                Log in
+              </button>
+            </Form>
+          ) : (
+            <div className="mt-2">
+              <h5 className="text-lg font-semibold">Hi there,</h5>
+              <p>
+                {" "}
+                you are already setup! But feel free to check the steps again.
+              </p>
+            </div>
+          )}
+          {
+            <div
+              className={`${
+                !loaderData.cookie ? "md:mt-10 mt-2 " : "mt-2"
+              } text-sm `}
+            >
+              <p className="">
+                *NOTE* We do not use your username and password for anything
+                except changing the wordpress image SEO.
+              </p>
+            </div>
+          }
+          <div>
+            {actionData?.error_message?.map((message: any, id: any) => (
+              <p key={id}>{message}</p>
+            ))}
           </div>
-        }
-        <div>
-          {actionData?.error_message?.map((message: any, id: any) => (
-            <p key={id}>{message}</p>
-          ))}
         </div>
       </div>
     </div>
