8d3210c38a46a0bb976a2696f0ae73743f630ffd
IndieAK <kindbergadam@gmail.com>
2023-04-19 09:34:02 +0200

    added comment to filter component

--- app/components/filter.tsx
+++ app/components/filter.tsx
@@ -3,6 +3,7 @@ import { Form } from "@remix-run/react";
 export const Filter = () => {
   return (
     <Form>
+      {/*Image type filter, Date filter using ai-generated-date, Date filter using upload date, search bar */}
       <label htmlFor="image-types">Image Type</label>
       <select name="image-types">
         <option>All Images</option>
