78b00a3f2a59b3ce1ad075aed142924607e0d64d
IndieAK <kindbergadam@gmail.com>
2023-04-24 13:46:32 +0200

    added footer component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,4 @@
-import { Link } from "@remix-run/react";
-import Btn from "~/components/button";
+import { Footer } from "~/components/footer";
 import { Hero } from "~/components/hero";
 import InfoCards from "~/components/infoCards";
 import Navbar from "~/components/navbar";
@@ -14,7 +13,7 @@ export default function Index() {
               ipsum ducimus ipsa maiores, error totam doloremque illum corrupti
               perferendis"
       />
-      <div className="container px-5 pb-40 mx-auto max-w-7xl">
+      <div className="container px-5 mx-auto pb-14 max-w-7xl">
         <div>
           <h1 className="mt-20 text-4xl font-semibold text-center lg:text-5xl">
             Why should you use us
@@ -39,19 +38,11 @@ export default function Index() {
             dolor, sit amet consectetur adipisicing elit. Officia ea quos,
             iste culpa nam eligendi aliquam Lorem ipsum dolor, sit amet"
         />
-        <div className="px-10 py-8 mx-auto mt-20 bg-black sm:py-16 sm:px-20 rounded-3xl">
-          <div className="container mx-auto">
-            <h1 className="font-bold tracking-wider text-center text-white sm:text-4xl md:text-5xl ">
-              Try out our SEO enhancer.
-            </h1>
-            <p className="mt-5 text-xs text-center text-gray-400 sm:text-sm">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
-              ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-
-            <Btn title="Setup" link href="/setup" />
-          </div>
-        </div>
+        <Footer
+          title="Try out our SEO enhancer."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit."
+        />
       </div>
     </>
   );
