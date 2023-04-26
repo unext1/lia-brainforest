56b93e34e958e25e60001ddbe198b18907181055
IndieAK <kindbergadam@gmail.com>
2023-04-26 14:08:35 +0200

    removed supabase

--- app/cookie.ts
+++ app/cookie.ts
@@ -1,4 +1,3 @@
 import { createCookie } from "@remix-run/node"; // or cloudflare/deno
 
 export const wordpressCookie = createCookie("wordpress", {});
-export const authCookie = createCookie("supabase-auth-token", {});
