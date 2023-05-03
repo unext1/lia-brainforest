37fc45c2a5b152eb598aa61b27f5635b525648f5
IndieAK <kindbergadam@gmail.com>
2023-05-03 14:07:32 +0200

    fixed errors

--- app/routes/dashboard/workplaces/$workplaceId/index.tsx
+++ app/routes/dashboard/workplaces/$workplaceId/index.tsx
@@ -1,15 +1,9 @@
 import { Users } from "~/components/workplace/users";
 import type { LoaderArgs } from "@remix-run/node";
 import { requireUser } from "~/services/auth.server";
-import { GetUsersFromWorkplace } from "~/services/hasura.server";
 export async function loader({ request, params }: LoaderArgs) {
   const user = await requireUser(request);
   const { workplaceId } = params;
-  console.log(workplaceId, user?.token!, "LOADEr");
-  const workplaceUsers = await GetUsersFromWorkplace({
-    token: user?.token!,
-    workplaceId: workplaceId!,
-  });
   return {};
 }
 export default function Index() {
