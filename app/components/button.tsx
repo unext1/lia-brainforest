8a1fa9ee4a0db64f4eb9f336a1437fbd43eaa00f
IndieAK <kindbergadam@gmail.com>
2023-05-04 16:09:29 +0200

    fixed filter styling

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -7,13 +7,13 @@ const Btn = ({
 }: {
   title: string;
   link: boolean;
-  href: string;
+  href?: string;
 }) => {
   return (
     <>
       {link ? (
         <Link
-          to={href}
+          to={href!}
           className="w-fit duration-150 transform hover:scale-105 flex sm:px-12 sm:py-2.5 mx-auto mt-8 text-xs px-6 py-1.5 md:text-sm font-bold text-white uppercase bg-red-500 rounded-lg"
         >
           {title}
