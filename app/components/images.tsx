a0c8b2b178c6b63acaaf370d60cc3b764c662137
laurynas <unextbusinessman@gmail.com>
2023-05-05 11:20:30 +0200

    filtering

--- app/components/images.tsx
+++ app/components/images.tsx
@@ -1,49 +1,47 @@
-import { Link } from '@remix-run/react';
-import { forwardRef } from 'react';
-import { type WPschema } from '~/types';
+import { Link } from "@remix-run/react";
+import { forwardRef } from "react";
+import { type WPschema } from "~/types";
 
 type Props = {
-    data: WPschema[];
-    navigation: any;
-    error_message: string;
-    workplaceId: string;
+  data: WPschema[];
+  navigation: any;
+  error_message: string;
+  workplaceId: string;
 };
 const Images = forwardRef<HTMLDivElement, Props>(function Images(
-    { data, navigation, workplaceId },
-    ref
+  { data, navigation, workplaceId },
+  ref
 ) {
-    return (
-        <div className='w-full p-5 mt-5 bg-white xl:mt-10 rounded-xl max-h-[80vh]'>
+  return (
+    <div className="w-full m-2  mt-5 bg-white xl:mt-10 rounded-xl max-h-[80vh]">
+      <div
+        ref={ref}
+        className=" flex xl:block xl:h-[700px] xl:py-5  overflow-x-auto snap-x space-x-10 xl:space-x-0 xl:overflow-y-auto  snap-mandatory xl:snap-y xl:space-y-10 max-h-[70vh]"
+      >
+        {data?.map((image) => (
+          <div
+            key={image.id}
+            className="relative flex-shrink-0 p-5 snap-center"
+          >
             <div
-                ref={ref}
-                className=' flex xl:block xl:h-[700px] xl:py-10  overflow-x-auto snap-x space-x-10 xl:space-x-0 xl:overflow-y-auto  snap-mandatory xl:snap-y xl:space-y-10 max-h-[70vh]'
+              className={` absolute top-5 w-4 h-4 rounded-full right-4 ${
+                image.ai_generated_text ? "bg-green-500" : "bg-red-500"
+              }`}
+            ></div>
+            <Link
+              to={`/dashboard/workplaces/${workplaceId}/images/${image.id}${navigation}`}
             >
-                {data?.map((image) => (
-                    <div
-                        key={image.id}
-                        className='relative flex-shrink-0 snap-center'
-                    >
-                        <div
-                            className={` absolute top-0 w-4 h-4  rounded-full right-0 ${
-                                image.ai_generated_text
-                                    ? 'bg-green-500'
-                                    : 'bg-red-500'
-                            }`}
-                        ></div>
-                        <Link
-                            to={`/dashboard/workplaces/${workplaceId}/images/${image.id}${navigation}`}
-                        >
-                            <img
-                                src={image.source_url}
-                                alt={image.source_url}
-                                className='object-fill w-24 h-24 '
-                            />
-                        </Link>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+              <img
+                src={image.source_url}
+                alt={image.source_url}
+                className="object-fill w-24 h-24 "
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 });
 
 export default Images;
--- app/routes/dashboard/workplaces/$workplaceId/images/$img.tsx
+++ app/routes/dashboard/workplaces/$workplaceId/images/$img.tsx
@@ -1,173 +1,173 @@
-import { json, type ActionArgs, type LoaderArgs } from '@remix-run/node';
+import { json, type ActionArgs, type LoaderArgs } from "@remix-run/node";
 import {
-    Form,
-    useActionData,
-    useLoaderData,
-    useNavigation,
-} from '@remix-run/react';
-import { useEffect } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-import { ImageComponent } from '~/components/imageform';
-import { requireUser } from '~/services/auth.server';
-import { GetWorkplaceById } from '~/services/hasura.server';
-import { type WPschema } from '~/types';
+  Form,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "@remix-run/react";
+import { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { ImageComponent } from "~/components/imageform";
+import { requireUser } from "~/services/auth.server";
+import { GetWorkplaceById } from "~/services/hasura.server";
+import { type WPschema } from "~/types";
 
 export async function action({ request, params }: ActionArgs) {
-    const { workplaceId } = params;
+  const { workplaceId } = params;
 
-    const formData = await request.formData();
-    const values = Object.fromEntries(formData);
+  const formData = await request.formData();
+  const values = Object.fromEntries(formData);
 
-    //FIX WITH ZODIX IF POSSIBLE
-    const aiInputs = {
-        title: values.title, //check if empty
-        alt_text: values.description,
-        description: values.description,
-    };
+  //FIX WITH ZODIX IF POSSIBLE
+  const aiInputs = {
+    title: values.title, //check if empty
+    alt_text: values.description,
+    description: values.description,
+  };
 
-    const user = await requireUser(request);
+  const user = await requireUser(request);
 
-    try {
-        const workplace = await GetWorkplaceById({
-            token: user?.token!,
-            id: workplaceId!,
-        });
-        await fetch(`${workplace.url}wp-json/wp/v2/media/${values.id}`, {
-            method: 'POST',
-            headers: {
-                Authorization: `Basic ${workplace.token}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(aiInputs),
-        });
-        return { toastMessage: 'Text has been changed !' };
-    } catch (error) {
-        return json({ toastMessage: 'There was an error' });
-    }
+  try {
+    const workplace = await GetWorkplaceById({
+      token: user?.token!,
+      id: workplaceId!,
+    });
+    await fetch(`${workplace.url}wp-json/wp/v2/media/${values.id}`, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${workplace.token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(aiInputs),
+    });
+    return { toastMessage: "Text has been changed !" };
+  } catch (error) {
+    return json({ toastMessage: "There was an error" });
+  }
 }
 
 export async function loader({ params, request }: LoaderArgs) {
-    const { img, workplaceId } = params;
+  const { img, workplaceId } = params;
 
-    const user = await requireUser(request);
+  const user = await requireUser(request);
 
-    try {
-        const workplace = await GetWorkplaceById({
-            token: user?.token!,
-            id: workplaceId!,
-        });
+  try {
+    const workplace = await GetWorkplaceById({
+      token: user?.token!,
+      id: workplaceId!,
+    });
 
-        const imageResponse = await fetch(
-            `${workplace.url}wp-json/wp/v2/media/${img}`
-        );
+    const imageResponse = await fetch(
+      `${workplace.url}wp-json/wp/v2/media/${img}`
+    );
 
-        const data: WPschema = await imageResponse.json();
-        const aiResponse = await fetch(
-            'https://northeurope.api.cognitive.microsoft.com/vision/v3.2/describe?maxCandidates=1&language=en&model-version=latest',
-            {
-                method: 'POST',
-                headers: {
-                    'Ocp-Apim-Subscription-Key': process.env
-                        .AZURE_SUBSCRIPTION_KEY as string,
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    url: data.source_url,
-                }),
-            }
-        );
+    const data: WPschema = await imageResponse.json();
+    const aiResponse = await fetch(
+      "https://northeurope.api.cognitive.microsoft.com/vision/v3.2/describe?maxCandidates=1&language=en&model-version=latest",
+      {
+        method: "POST",
+        headers: {
+          "Ocp-Apim-Subscription-Key": process.env
+            .AZURE_SUBSCRIPTION_KEY as string,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          url: data.source_url,
+        }),
+      }
+    );
 
-        const aiData = await aiResponse.json();
+    const aiData = await aiResponse.json();
 
-        if (!aiData) return json({ tags: '', description: '' });
+    if (!aiData) return json({ tags: "", description: "" });
 
-        const aiTextDescription: string = aiData.description.captions
-            ? aiData.description.captions.map((caption: any) => caption.text)
-            : '';
+    const aiTextDescription: string = aiData.description.captions
+      ? aiData.description.captions.map((caption: any) => caption.text)
+      : "";
 
-        const wordpressDescription =
-            data.description.rendered.split('</p>').length > 1
-                ? data.description.rendered.split('</p>')[1].slice(4)
-                : '';
+    const wordpressDescription =
+      data.description.rendered.split("</p>").length > 1
+        ? data.description.rendered.split("</p>")[1].slice(4)
+        : "";
 
-        return json({
-            tags: aiData.description.tags,
-            description: aiTextDescription,
-            data: { ...data, description: { rendered: wordpressDescription } },
-            message: 'hi',
-        });
-    } catch (err: any) {
-        if (err.code === 'ERR_INVALID_URL')
-            return {
-                error_message: 'Invalid Url',
-            };
-        return { error_message: 'Error' };
-    }
+    return json({
+      tags: aiData.description.tags,
+      description: aiTextDescription,
+      data: { ...data, description: { rendered: wordpressDescription } },
+      message: "hi",
+    });
+  } catch (err: any) {
+    if (err.code === "ERR_INVALID_URL")
+      return {
+        error_message: "Invalid Url",
+      };
+    return { error_message: "Error" };
+  }
 }
 const ImageForm = () => {
-    const toastMessage = useActionData();
+  const toastMessage = useActionData();
 
-    const { data, error_message, tags, description } = useLoaderData<{
-        tags: string | string[];
-        description: string;
-        data: WPschema;
-        error_message: string;
-    }>();
+  const { data, error_message, tags, description } = useLoaderData<{
+    tags: string | string[];
+    description: string;
+    data: WPschema;
+    error_message: string;
+  }>();
 
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        if (!toastMessage) {
-            return;
-        }
-        if (toastMessage) {
-            toast.success(toastMessage.toastMessage);
-        }
-    }, [toastMessage]);
+  useEffect(() => {
+    if (!toastMessage) {
+      return;
+    }
+    if (toastMessage) {
+      toast.success(toastMessage.toastMessage);
+    }
+  }, [toastMessage]);
 
-    return (
-        <>
-            {toastMessage?.toastMessage ? (
-                <Toaster position='top-right' reverseOrder={false} />
-            ) : null}
-            <div className='max-h-screen overflow-hidden'>
-                {error_message}
-                <div className='grid grid-cols-5 gap-10 mt-10 '>
-                    <div className='col-span-5 p-5 bg-white md:col-span-3 rounded-xl'>
-                        <div className='mb-5'>
-                            <p className='text-sm tracking-wider text-gray-600'>
-                                AI GENERATOR
-                            </p>
-                            <div className='text-lg font-semibold '>
-                                {data?.title.rendered.replace(/,/g, ' ')}
-                            </div>
-                        </div>
-                        <div className='flex items-center justify-center max-h-[70vh]'>
-                            <img
-                                src={data?.source_url}
-                                alt={data?.source_url}
-                                className='max-w-full max-h-[70vh] object-contain'
-                            />
-                        </div>
-                    </div>
+  return (
+    <>
+      {toastMessage?.toastMessage ? (
+        <Toaster position="top-right" reverseOrder={false} />
+      ) : null}
+      <div className="overflow-hidden ">
+        {error_message}
+        <div className="grid grid-cols-5 gap-5 mt-10 ">
+          <div className="col-span-5 p-10 bg-white md:col-span-3 rounded-xl">
+            <div className="mb-5">
+              <p className="text-sm tracking-wider text-gray-600">
+                AI GENERATOR
+              </p>
+              <div className="text-lg font-semibold ">
+                {data?.title.rendered.replace(/,/g, " ")}
+              </div>
+            </div>
+            <div className="flex items-center justify-center max-h-[50vh]">
+              <img
+                src={data?.source_url}
+                alt={data?.source_url}
+                className="max-w-full max-h-[50vh] object-contain"
+              />
+            </div>
+          </div>
 
-                    <div className='col-span-5 p-10 bg-white md:col-span-2 rounded-xl'>
-                        <Form method='post'>
-                            <ImageComponent
-                                tags={tags}
-                                data={data}
-                                navigation={navigation}
-                                description={description}
-                            />
-                        </Form>
-                        {/* {actionData && actionData.errormessage
+          <div className="col-span-5 p-10 bg-white md:col-span-2 rounded-xl">
+            <Form method="post">
+              <ImageComponent
+                tags={tags}
+                data={data}
+                navigation={navigation}
+                description={description}
+              />
+            </Form>
+            {/* {actionData && actionData.errormessage
               ? actionData.errormessage.code
               : null} */}
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ImageForm;
